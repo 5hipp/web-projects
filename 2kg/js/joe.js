3c035d920b9b3c5488d451e74894c2c21335a202
Joe <35581310+5hipp@users.noreply.github.com>
2022-03-16 14:41:38 +0000

    clean up
    cleaned up description content and some title and disabling js

--- 2kg/js/joe.js
+++ 2kg/js/joe.js
@@ -1,17 +1,15 @@
-window.oncontextmenu = function () {
-console.log("Right Click Disabled");
+window.oncontextmenu = function() {
+    console.log("Right Click Disabled");
     return false;
 }
 
 window.onkeydown = function(evt) {
-    if(evt.keyCode == 123 || evt.keyCode == 85 || evt.keyCode == 17 || evt.keyCode == 16 || evt.keyCode == 74 || evt.keyCode == 116 || evt.keyCode == 73) return false;
+    if (evt.keyCode == 123 || evt.keyCode == 85 || evt.keyCode == 17 || evt.keyCode == 16 || evt.keyCode == 74 || evt.keyCode == 116 || evt.keyCode == 73) return false;
+    console.log("Console Keys  Disabled");
 };
 
-window.onkeypress = function(evt) {
-    if(evt.keyCode == 123 || evt.keyCode == 85 || evt.keyCode == 17 || evt.keyCode == 16 || evt.keyCode == 74 || evt.keyCode == 116 || evt.keyCode == 73) return false;
+function disableF5(e) {
+    if ((e.which || e.keyCode) == 116) e.preventDefault(); 
+    console.log("Refresh Disabled");
 };
-
-function disableF5(e) { if ((e.which || e.keyCode) == 116) e.preventDefault(); };
 $(document).on("keydown", disableF5);
-
-
