0e6ad5a008bc60b6c703c5a5103b2cf852e222ae
Joe <joeshipp@protonmail.com>
2023-04-04 16:25:00 +0200

    implementing new theme files
    new files for the new base theme flatty have been implemented

---
+++ bug-hunter-website/forum/inc/languages/english/avatarep.lang.php
@@ -0,0 +1,19 @@
+<?php
+/**
+*  Language English
+*  Last Poster Avatar 3.0
+*
+*  Sitio Web: https://soportemybb.es
+*  Autor: Whiteneo - neogeoman@gmail.com
+*/
+$l['avatarep_user_profile'] = "View Profile";
+$l['avatarep_user_messages'] = "View Messages";
+$l['avatarep_user_sendpm'] = "Send PM";
+$l['avatarep_user_sendemail'] = "Send E-mail";
+$l['avatarep_user_threads'] = "View Threads";
+$l['avatarep_user_error'] = " Error ";
+$l['avatarep_user_error_text'] = " You must login to see this content ";
+$l['avatarep_user_alt'] = "{1}'s Avatar";
+$l['avatarep_user_no_avatar'] = "No avatar";
+$l['avatarep_retrieving'] = "Retrieving Data";
+$l['avatarep_loading'] = "Loading...";
---
+++ bug-hunter-website/forum/inc/languages/espanol/admin/avatarep.lang.php
@@ -0,0 +1,80 @@
+<?php
+/**
+*  Idioma Español
+*  Last Poster Avatar
+*
+*  Sitio Web: https://soportemybb.es
+*  Autor: Whiteneo - neogeoman@gmail.com
+*/
+
+// No remover estos valores
+$l['avatarep_name'] = "Last Poster Avatar";
+$l['avatarep_descrip'] = "Muestra el avatar del usuario en casi todas las zonas de los foros donde no existe por defecto.";
+$l['avatarep_config'] = "Configurar Plugin";
+$l['avatarep_style_usernames'] = "Tienes instalado el plugin Style Usernames Plugin (Deshabilita elajuste formateo de nombres de usuario en losajustes delplugin de avatares, o desinstala el plugin Style Usernames para evitar errores conocidos.)";
+$l['avatarep_disabled_msg'] = "Plugin deshabilitado";
+
+// Frases de la configuracion del plugin
+$l['avatarep_title'] = "[Plugin] Last Poster Avatar";
+$l['avatarep_title_descrip'] = "Configurar opciones del plugin";
+
+$l['avatarep_power'] = "Activar Plugin";
+$l['avatarep_power_descrip'] = "Selecciona si deseas activar el plugin.";
+
+$l['avatarep_forum'] = "Mostrar Avatar en Foros";
+$l['avatarep_forum_descrip'] = "Muestra el avatar del usuario en la lista de Foros";
+
+$l['avatarep_thread_owner'] = "Mostrar Avatar en Temas";
+$l['avatarep_thread_owner_descrip'] = "Muestra el avatar del creador del Tema";
+
+$l['avatarep_thread_lastposter'] = "Mostrar Avatar en Temas";
+$l['avatarep_thread_lastposter_descrip'] = "Muestra el avatar del ultimo que contesto al mensaje";
+
+$l['avatarep_thread_contributor'] = "Mostrar Avatar del Contribuyente en Temas";
+$l['avatarep_thread_contributor_descrip'] = "Muestra el avatar del creador del tema en la lista de temas, o si has respondido el tuyo";
+
+$l['avatarep_thread_announcements'] = "Mostrar Avatar en Anuncios";
+$l['avatarep_thread_announcements_descrip'] = "Muestra el avatar del usuario que creo los anuncios";
+
+$l['avatarep_portal'] = "Mostrar avatar en el portal";
+$l['avatarep_portal_descrip'] = "Mostrar el avatar en el portal, últimos mensajes y anuncios del portal";
+
+$l['avatarep_search'] = "Mostrar avatar en búsquedas";
+$l['avatarep_search_descrip'] = "Mostrar el avatar en los resultados encontrados de mensajes y temas";
+
+$l['avatarep_menu'] = "Menú Popup";
+$l['avatarep_menu_descrip'] = "Muestra un menú con datos del usuario al hacer clic en su avatar";
+
+$l['avatarep_menu_events'] = "Evento de la ventana";
+$l['avatarep_menu_events_descrip'] = "Elige el modo en que se mostrará la ventana (Evento del ratón).";
+
+$l['avatarep_letter_avatar'] =  "Letras en lugar del avatar por defecto";
+$l['avatarep_letter_avatar_descrip'] = "Elige No si deseas dejar el avatar por defecto de mybb en lugar de letras para usuarios sin avatares";
+
+$l['avatarep_format'] = "Nombres de usuario formateados";
+$l['avatarep_format_descrip'] = "Muestra los nombres de usuario con códigos html configurados en los grupos de usuario";
+
+$l['avatarep_private'] = "Avatar en mensajes privados";
+$l['avatarep_private_descrip'] = "Mostrar avatar en mensajes privados";
+
+$l['avatarep_latest_threads'] = "Avatar en últimos tema";
+$l['avatarep_latest_threads_descrip'] = "Muestra el avatar de los últimos temas en el Portal y en el índice si existe el plugin SideBox";
+
+$l['avatarep_thread_lastposter_mark'] = "Marcar mis temas respondidos";
+$l['avatarep_thread_lastposter_mark_descrip'] = "Mostrar el avatar del usuario en los temas creados, encima del avatar del creador, para saber a que temas he respondido, (requiere consultas adicionales).";
+
+$l['avatarep_usercp'] = "Avatar en Panel de Control";
+$l['avatarep_usercp_descrip'] = "Muestra el avatar en la lista de temas de tu panel de control";
+
+$l['avatarep_usercp_fsubs'] = "Avatar para foros suscritos en el panel de control";
+$l['avatarep_usercp_fsubs_desc'] = "Deshabilita este ajuste si no quieres mostrar el avatar en la lista de foros donde estas suscrito desde el panel de control de usuario";
+
+$l['avatarep_similar_threads'] = "Avatar en temas similares desde la lista de temas";
+$l['avatarep_similar_threads_desc'] = "Deshabilita si no quieres mostrar el avatar en la lista de temas similares al mostrar los temas (Debe habilitar Temas similares)";
+
+$l['avatarep_usercp_tsubs'] = "Mostrar avatar para los temas suscritos desde el panel de control";
+$l['avatarep_usercp_tsubs_desc'] = "Deshabilita si no quieres mostrar el avatar en la lista de tus temas suscritos desde el panel de control del usuario";
+
+$l['avatarep_onerror'] = "Mostrar avatares rotos o sin avatar como el avatar por defecto";
+$l['avatarep_onerror_descrip'] = "Si algún error ocurre en la carga de los avatares, como la imágen ya no está disponible, o no carga correctamente o simplemente no responde el servidor donde se aloja, entonces cargamos el avatar por defecto (Esto rompe la validación de la w3c debido a que la función onerror de la etiqueta img no es validada, pero existe. Si no desea esto deshabilite esta opción).";
+
